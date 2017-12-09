272de3f14236ec30eb16ad8f6f1f65ebd11612ac
tagasouichiro <stitow14@gmail.com>
2017-12-09 22:13:08 +0900

    Add ver2

--- main.go
+++ main.go
@@ -9,6 +9,8 @@ func main() {
 
 	res := initialShingle(min, max, arr)
 	fmt.Println(res)
+	res = shingle2(min, max, arr)
+	fmt.Println(res)
 }
 
 func merge(arr []string) string {
@@ -34,5 +36,38 @@ func initialShingle(min, max int, arr []string) []string {
 			res = append(res, merge(arr[i:j+cmin]))
 		}
 	}
+	return res
+}
+
+func shingle2(min, max int, arr []string) []string {
+	if min > max {
+		return []string{}
+	}
+
+	// for alloc array
+	if max > len(arr) {
+		max = len(arr)
+	}
+	maxLen := max - min + 1
+	resLen := 0
+	for maxLen > 0 {
+		resLen += maxLen
+		maxLen--
+	}
+	fmt.Println(resLen)
+	res := make([]string, resLen)
+
+	idx := 0
+	for i := 0; i < len(arr); i++ {
+		cmin := min
+		for j := i; j+cmin < len(arr)+1; j++ {
+			if len(arr[i:j+cmin]) > max {
+				break
+			}
+			res[idx] = merge(arr[i : j+cmin])
+			idx++
+		}
+	}
+
 	return res
 }
--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -21,6 +22,10 @@ var cases = []struct {
 	{
 		in:  in{min: 2, max: 2, arr: []string{"a", "b", "c", "d", "e"}},
 		out: []string{"ab", "bc", "cd", "de"},
+	},
+	{
+		in:  in{min: 2, max: 3, arr: []string{"a", "b", "c", "d", "e"}},
+		out: []string{"ab", "abc", "bc", "bcd", "cd", "cde", "de"},
 	},
 	{
 		in:  in{min: 2, max: 5, arr: []string{"a", "b", "c", "d", "e"}},
@@ -55,9 +60,44 @@ func contains(str string, arr []string) bool {
 	return false
 }
 
+func initial() []string {
+	s := "aa"
+	arr := make([]string, 1000)
+	for i := 0; i < 1000; i++ {
+		arr[i] = s
+	}
+	return arr
+}
+
 func TestInitialShingle(t *testing.T) {
 	for _, c := range cases {
 		res := initialShingle(c.in.min, c.in.max, c.in.arr)
 		equal(t, res, c.out)
+	}
+}
+
+func BenchmarkInitialShingle(b *testing.B) {
+	arr := initial()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		initialShingle(2, 100, arr)
+	}
+}
+
+func TestShingle2(t *testing.T) {
+	for _, c := range cases {
+		res := shingle2(c.in.min, c.in.max, c.in.arr)
+		fmt.Println(res)
+		equal(t, res, c.out)
+	}
+}
+
+func BenchmarkShingle2(b *testing.B) {
+	arr := initial()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		shingle2(2, 100, arr)
 	}
 }
