2018836e623d57d725cc262a4edbb76c68a9066f
tagasouichiro <stitow14@gmail.com>
2017-12-11 23:05:08 +0900

    Fix max

--- main.go
+++ main.go
@@ -79,7 +79,7 @@ func shingle3(min, max int, arr []string) []string {
 
 	for i := 0; i < len(arr); i++ {
 		cmin := min
-		for j := i; j+cmin < len(arr)+1; j++ {
+		for j := i; j+cmin <= len(arr); j++ {
 			if len(arr[i:j+cmin]) > max {
 				break
 			}
